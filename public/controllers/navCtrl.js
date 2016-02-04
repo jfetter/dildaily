b79e44ed5837928c562881acd45b3400a1106899
Jillian Fetter <jillian.fetter@gmail.com>
2016-02-03 21:09:54 -0800

    now have full crud, sort and search but have not been successful in setting up predictive searching.

--- public/controllers/navCtrl.js
+++ public/controllers/navCtrl.js
@@ -2,12 +2,8 @@
 
 angular.module("myApp")
 
-.controller("navCtrl", function($scope, $state, UtilityService){
+.controller("navCtrl", function($scope, $rootScope, $state, UtilityService){
 	// hide login or logout button
-
-		function twoWaySearch(catergory){
-
-		}
 
 	$scope.loginButton = function(){
 		return UtilityService.loggedIn();
@@ -35,8 +31,72 @@ angular.module("myApp")
 	$scope.searchTasks = function(){
 		console.log("TASK", $scope.searchFor);
 		console.log("cat", $scope.searchCat);
-		twoWaySearch($scope.searchCat);
+		var cat;
+		var searchTerm;
+		$scope.searchResults = [];
+		if ($scope.searchCat === 'tasks'){
+			cat = $rootScope.tasks;
+			searchTerm = "task_name";
+		} else if ($scope.searchCat === 'archives'){
+			cat = $rootScope.archives
+			searchTerm;
+			console.log("archives coming soon")
+		}else if ($scope.searchCat === 'contacts'){
+			cat = $rootScope.contacts
+			searchTerm;
+			console.log("contacts coming soon")
+		}else if ($scope.searchCat === 'tools'){
+			cat = $rootScope.contacts
+			searchTerm;
+			console.log("prolly be a while til I have tools")
+		}
+			cat.forEach(function(item){
+				if (item[searchTerm].match($scope.searchFor))
+				$scope.searchResults.push(item[searchTerm])
+			})
+			console.log($scope.searchFor);
+	}
 
+// 			$scope.$watch("searchFor", function(newVal){
+// 			$searchResults = newVal;
+// 		})
+
+// 	// $scope.searchTasks = function(){
+// 	// 	console.log("TASK", $scope.searchFor);
+// 	// 	console.log("cat", searchCat);
+	
+// $scope.$watch("cat", function(searchCat)){
+// 		var searchTerm;
+// 		var searchArray
+// 		$scope.searchArray = [];
+// 		if (searchCat === 'tasks'){
+// 			searchArray = $rootScope.tasks;
+// 			searchTerm = "task_name";
+// 		} else if (searchCat === 'archives'){
+// 			searchArray = $rootScope.archives
+// 			searchTerm;
+// 			console.log("archives coming soon")
+// 		}else if (searchCat === 'contacts'){
+// 			searchArray = $rootScope.contacts
+// 			searchTerm;
+// 			console.log("contacts coming soon")
+// 		}else if (searchCat === 'tools'){
+// 			searchArray = $rootScope.contacts
+// 			searchTerm;
+// 			console.log("prolly be a while til I have tools")
+// 		}
+// 			// searchArray.forEach(function(item){
+// 			// 	if (item[searchTerm].match($scope.searchFor))
+// 			// 	$scope.searchArray.push(item[searchTerm])
+// 			// })
+// 			// console.log($scope.searchFor);
+// 			$scope.searchArray = searchArray[searchTerm];
+// 	 }
+// }
+
+	$scope.showDetails=function(result){
+		//$state.go(detailsPage);
+		console.log("build details view")
 	}
 
 })
