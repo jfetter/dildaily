79bcdc404a0fc73b8bab0363d01c5b229122687d
Jillian Fetter <jillian.fetter@gmail.com>
2016-02-03 22:34:23 -0800

    modularized search function

--- public/controllers/navCtrl.js
+++ public/controllers/navCtrl.js
@@ -26,76 +26,56 @@ angular.module("myApp")
 		$state.go('home');
 	};
 
-var	assembleSearch = function(searchArray, searchTerm){
+	$scope.searchArray = [];
+function assembleSearch(searchArray, searchTerm){
 	searchArray.forEach(function(item){
 		$scope.searchArray.push(item[searchTerm])
 	})
 }
 
-$scope.$watch("cat", function(cat, oldCat){
-		console.log("NEW CAT", cat)
-		var searchTerm;
-		var searchArray = [];
-		$scope.searchArray = [];
+var searchCases = function(cat){
 		if (cat === 'tasks'){
-			searchArray = $rootScope.tasks;
-			searchTerm = "task_name";
+			var searchArray = $rootScope.tasks;
+			var searchTerm = "task_name";
 		} else if (cat === 'archives'){
-			searchArray = $rootScope.archives
-			searchTerm;
+			var searchArray = $rootScope.archives
+			var searchTerm;
 			console.log("archives coming soon")
 		}else if (cat === 'contacts'){
-			searchArray = $rootScope.contacts
-			searchTerm;
+			var searchArray = $rootScope.contacts
+			var searchTerm;
 			console.log("contacts coming soon")
 		}else if (cat === 'tools'){
-			searchArray = $rootScope.contacts
-			searchTerm;
+			var searchArray = $rootScope.contacts
+			var searchTerm;
 			console.log("prolly be a while til I have tools")
 		}
-			assembleSearch(searchArray, searchTerm)
-			//console.log($scope.searchFor);
-			//$scope.searchArray = searchArray[searchTerm];
-			//console.log($scope.searchArray);
+		return {searchArray: searchArray, searchTerm: searchTerm}
+}
+
+$scope.$watch("cat", function(cat, oldCat){
+	var searchObject = searchCases(cat);
+	var searchArray = searchObject.searchArray;
+	var searchTerm = searchObject.searchTerm; 
+	assembleSearch(searchArray, searchTerm)
+	//console.log($scope.searchFor);
+	//$scope.searchArray = searchArray[searchTerm];
+	//console.log($scope.searchArray);
 		})
 
 		console.log("cat", $scope.cat);
 
-	// $scope.searchTasks = function(){
-	// 	console.log("TASK", $scope.searchFor);
-	// 	console.log("cat", $scope.searchCat);
-	// 	var cat;
-	// 	var searchTerm;
-	// 	$scope.searchResults = [];
-	// 	if ($scope.searchCat === 'tasks'){
-	// 		cat = $rootScope.tasks;
-	// 		searchTerm = "task_name";
-	// 	} else if ($scope.searchCat === 'archives'){
-	// 		cat = $rootScope.archives
-	// 		searchTerm;
-	// 		console.log("archives coming soon")
-	// 	}else if ($scope.searchCat === 'contacts'){
-	// 		cat = $rootScope.contacts
-	// 		searchTerm;
-	// 		console.log("contacts coming soon")
-	// 	}else if ($scope.searchCat === 'tools'){
-	// 		cat = $rootScope.contacts
-	// 		searchTerm;
-	// 		console.log("prolly be a while til I have tools")
-	// 	}
-	// 		cat.forEach(function(item){
-	// 			if (item[searchTerm].match($scope.searchFor))
-	// 			$scope.searchResults.push(item[searchTerm])
-	// 		})
-	// 		console.log($scope.searchFor);
-	// }
+$scope.searchTasks = function(){
+	var cat = $scope.cat;
+	var searchObject = searchCases(cat);
+	var searchArray = searchObject.searchArray;
+	var searchTerm = searchObject.searchTerm;
+	assembleSearch(searchArray, searchTerm);
+}
 
-// 			$scope.$watch("searchFor", function(newVal){
-// 			$searchResults = newVal;
-// 		})
-
-
-	
+		// 	$scope.$watch("searchFor", function(newVal){
+		// 	$searchResults = newVal;
+		// })
 
 
 	$scope.showDetails=function(result){
