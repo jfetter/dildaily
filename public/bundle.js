c56cff043fce391773fcc69d3f1f5b9d5e50f768
Jillian Fetter <jillian.fetter@gmail.com>
2016-03-03 23:04:52 -0700

    not sure why task names do not display on heroku

--- public/bundle.js
+++ public/bundle.js
@@ -3637,6 +3637,50 @@ angular.module("toWork", ["ui.router", "ui.bootstrap", "satellizer", "ngAnimate"
     return _moment;
 
 }));
+"use strict";
+
+angular.module("toWork")
+
+.directive("right-view",function(){
+	return{
+		templateUrl: "templates/right-view.html"
+	}
+})
+
+.directive('leftView', function(){
+  return{
+    templateUrl: "directives/left-view.html"
+  }
+})
+
+.directive('taskForm', function(){
+  return{
+    templateUrl: "directives/task-form.html"
+  }
+})
+  .directive('contactForm', function(){
+  return{
+    templateUrl: "directives/contact-form.html"
+  }
+})
+
+.directive('mainTable', function(){
+  return{
+    templateUrl: "directives/main-table.html"
+  }
+})
+
+// .directive('tools', function(){
+//   return{
+//     templateUrl: "directives/tools.html"
+//   }
+// })
+
+// .directive('taskModal', function(){
+//   return{
+//     templateUrl: "partials/task-modal.html"
+//   }
+// })
 
 "use strict";
 
@@ -4778,50 +4822,6 @@ angular.module("toWork")
 
 
 })
-"use strict";
-
-angular.module("toWork")
-
-.directive("right-view",function(){
-	return{
-		templateUrl: "templates/right-view.html"
-	}
-})
-
-.directive('leftView', function(){
-  return{
-    templateUrl: "directives/left-view.html"
-  }
-})
-
-.directive('taskForm', function(){
-  return{
-    templateUrl: "directives/task-form.html"
-  }
-})
-  .directive('contactForm', function(){
-  return{
-    templateUrl: "directives/contact-form.html"
-  }
-})
-
-.directive('mainTable', function(){
-  return{
-    templateUrl: "directives/main-table.html"
-  }
-})
-
-// .directive('tools', function(){
-//   return{
-//     templateUrl: "directives/tools.html"
-//   }
-// })
-
-// .directive('taskModal', function(){
-//   return{
-//     templateUrl: "partials/task-modal.html"
-//   }
-// })
 
 "use strict";
 
